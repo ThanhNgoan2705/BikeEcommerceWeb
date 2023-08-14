6dea60e36ba6e31ed1e7eeb0e5ae0fd781ea923f
Thanh_Ngoan <20130335@st.hcmuaf.edu.vn>
2023-08-14 22:19:02 +0700

    add product

--- src/main/java/hcmuaf/edu/vn/BikeEcommerce/test.java
+++ src/main/java/hcmuaf/edu/vn/BikeEcommerce/test.java
@@ -1,26 +1,12 @@
 package hcmuaf.edu.vn.BikeEcommerce;
 
-import hcmuaf.edu.vn.BikeEcommerce.service.UserService;
+import hcmuaf.edu.vn.BikeEcommerce.model.Product;
+import hcmuaf.edu.vn.BikeEcommerce.service.ProductService;
 
-public class test {
+public class Test {
     public static void main(String[] args) {
-
-
-        UserService userService = new UserService();
-        System.out.println(userService.loginByUserNameOrEmail("h@gmail.com","1234").toString());
-
-//        User user = userService.loginByEmail("h@gmail.com","1234");
-
-//        User user = new User(GenerateId.generateId(),"h@gmail.com","1234","hoanghailata", GenerateSalt.generateSalt());
-//       userService.insertUser(user);
-//        User user = userService.getUserByKey("ad666e82-58af-4b75-b39a-28cfe9e25d8f");
-//        System.out.println(user.toString());
-//        user.setEmail("nguyenhoanghai@gmail.com");
-//        userService.updateUser(user);
-//        userService.deleteUserById("1234");
-//        User users = userService.getUserByKey("1234");
-//        System.out.println(users.toString());
-//        System.out.println ("jdbc:mysql://" + getHost() + ":" + getPort() + "/" + getDatabaseName());
-//        System.out.println(DBProperties.getHost());
+        Product product = new Product("1", "Pega", 4500000);
+        ProductService.getInstance().insert(product);
+        System.out.println(product);
     }
 }
