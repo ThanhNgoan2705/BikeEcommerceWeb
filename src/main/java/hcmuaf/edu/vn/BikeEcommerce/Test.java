4792237e8098732c928dc97ed5b8740e01fefb50
Thanh_Ngoan <20130335@st.hcmuaf.edu.vn>
2023-08-14 23:24:49 +0700

    test insert category

--- src/main/java/hcmuaf/edu/vn/BikeEcommerce/Test.java
+++ src/main/java/hcmuaf/edu/vn/BikeEcommerce/Test.java
@@ -3,6 +3,7 @@ package hcmuaf.edu.vn.BikeEcommerce;
 import hcmuaf.edu.vn.BikeEcommerce.model.Category;
 import hcmuaf.edu.vn.BikeEcommerce.model.Product;
 import hcmuaf.edu.vn.BikeEcommerce.service.BrandService;
+import hcmuaf.edu.vn.BikeEcommerce.service.CategoryService;
 import hcmuaf.edu.vn.BikeEcommerce.service.ProductService;
 
 public class Test {
@@ -11,6 +12,7 @@ public class Test {
         Product product = new Product("1", "Pega", 4500000, "Pega","24 inch", "Thép", "1 năm", 1, 1, null, null, null, null, 1 );
         Category category = new Category("1", "Xe đạp thể thao", "chay em", null, null, 1,1);
         ProductService.getInstance().insert(product);
+        CategoryService.getInstance().insert(category);
         ProductService.getInstance().update(product);
         System.out.println(product);
     }
