94e1a1e5adc4593f10310093bd1d11c45414bf9e
HoangHaiPCHome <20130166@st.hcmuaf.edu.vn>
2023-08-20 20:58:50 +0700

    test and fix AddressDAO SQL

--- src/main/java/hcmuaf/edu/vn/BikeEcommerce/DAO/AddressDAO.java
+++ src/main/java/hcmuaf/edu/vn/BikeEcommerce/DAO/AddressDAO.java
@@ -4,6 +4,7 @@ import hcmuaf.edu.vn.BikeEcommerce.model.Address;
 import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.customizer.BindBean;
+import org.jdbi.v3.sqlobject.customizer.BindMethods;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
@@ -12,16 +13,14 @@ import java.util.List;
 @RegisterBeanMapper(Address.class)
 public interface AddressDAO {
 
-    @SqlQuery(ScirptSQL.getAddressByUserId)
-    List<Address> getAddressByUserId(@Bind("userId") String userId);
     @SqlQuery(ScirptSQL.getAddressByAddressId)
     Address getAddressByAddressId(@Bind("addressId") String addressId);
     @SqlQuery(ScirptSQL.getAllAddress)
     List<Address> getAllAddress();
     @SqlUpdate(ScirptSQL.insertAddress)
-    void insertAddress(@BindBean Address address);
+    void insertAddress(@BindMethods Address address);
     @SqlUpdate(ScirptSQL.updateAddress)
-    void updateAddress(@BindBean Address address);
+    void updateAddress(@BindMethods Address address);
     @SqlUpdate(ScirptSQL.deleteAddressById)
     void deleteAddressById(@Bind("addressId") String addressId);
 }
