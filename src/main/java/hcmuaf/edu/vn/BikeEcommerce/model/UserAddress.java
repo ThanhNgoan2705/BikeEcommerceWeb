aa2813ef2c2a466361b8c83b52b6ebf949c7be91
HoangHaiPCHome <20130166@st.hcmuaf.edu.vn>
2023-08-21 01:14:30 +0700

    delete table UserAddress.java

--- src/main/java/hcmuaf/edu/vn/BikeEcommerce/model/UserAddress.java
+++
@@ -1,9 +0,0 @@
-package hcmuaf.edu.vn.BikeEcommerce.model;
-
-public class UserAddress {
-    private String userId;
-    private String addressId;
-    private String createdAt;
-    private String updatedAt;
-
-}
