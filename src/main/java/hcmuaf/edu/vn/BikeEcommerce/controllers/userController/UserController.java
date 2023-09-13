eeef90aad42345f1c76aafc821f3aa4d8f882621
Nguyễn Hoàng Hải <103479379+NgHgHai@users.noreply.github.com>
2023-09-14 06:17:08 +0700

    chuyen trang den user_detail và filter user

--- src/main/java/hcmuaf/edu/vn/BikeEcommerce/controllers/userController/UserController.java
+++ src/main/java/hcmuaf/edu/vn/BikeEcommerce/controllers/userController/UserController.java
@@ -1,8 +1,11 @@
-package hcmuaf.edu.vn.BikeEcommerce.controllers;
+package hcmuaf.edu.vn.BikeEcommerce.controllers.userController;
+
+import hcmuaf.edu.vn.BikeEcommerce.model.User;
+import hcmuaf.edu.vn.BikeEcommerce.model.sercurity.Token;
+import hcmuaf.edu.vn.BikeEcommerce.service.UserService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,14 +15,14 @@ import java.io.IOException;
 public class UserController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Cookie cookie = req.getCookies()[0];
-        req.setAttribute("cookies",cookie.getValue() );
-        System.out.println(cookie.getValue()+" user cookies");
+        Token token = (Token) req.getAttribute("token");
+        User user = UserService.getInstance().getUserByKey(token.getUserId());
+        req.setAttribute("user", user);
         req.getRequestDispatcher("/dev/User.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doGet(req , resp);
+        doGet(req, resp);
     }
 }
--- src/main/java/hcmuaf/edu/vn/BikeEcommerce/filter/UserFilter.java
+++ src/main/java/hcmuaf/edu/vn/BikeEcommerce/filter/UserFilter.java
@@ -1,13 +1,16 @@
 package hcmuaf.edu.vn.BikeEcommerce.filter;
 
 
+import hcmuaf.edu.vn.BikeEcommerce.model.sercurity.Token;
+import hcmuaf.edu.vn.BikeEcommerce.toolSecurity.TokenService;
+
 import javax.servlet.*;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.security.NoSuchAlgorithmException;
 
-public class TokenFilter implements Filter {
+public class UserFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         Filter.super.init(filterConfig);
@@ -15,20 +18,19 @@ public class TokenFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        Token token = null;
         Cookie cookie = ((HttpServletRequest) servletRequest).getCookies()[0];
         System.out.println(cookie.getValue() + " filter cookies");
-        PrintWriter printWriter = null;
-        if (cookie.getValue().equals("tokenFake")) {
-            servletRequest.getRequestDispatcher("/user").forward(servletRequest, servletResponse);
-        } else {
-            printWriter = servletResponse.getWriter();
-            printWriter.println("<script>\n" +
-                    "    alert(\"You are not logged in\");\n" +
-                    "</script>");
-            System.out.println("You are not logged in");
-            return;
+        String dataToken = cookie.getValue();
+        try {
+            token = TokenService.getInstance().getTokenFromHeader(dataToken);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
         }
-
+        if (token == null) {
+            servletRequest.getRequestDispatcher("/index.jsp").forward(servletRequest, servletResponse);
+        }
+        servletRequest.setAttribute("token", token);
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
