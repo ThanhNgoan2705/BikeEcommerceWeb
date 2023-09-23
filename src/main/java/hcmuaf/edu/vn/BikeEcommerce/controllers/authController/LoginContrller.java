43e45488a6d6a232af858a812ce1ff19f5d983b6
Nguyễn Hoàng Hải <103479379+NgHgHai@users.noreply.github.com>
2023-09-24 00:13:28 +0700

    them thư vien đe fix bug bao mat

--- src/main/java/hcmuaf/edu/vn/BikeEcommerce/controllers/authController/LoginContrller.java
+++ src/main/java/hcmuaf/edu/vn/BikeEcommerce/controllers/authController/LoginContrller.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 
 @WebServlet("/login")
 
@@ -49,6 +50,8 @@ public class LoginContrller extends HttpServlet {
             } catch (NoSuchAlgorithmException e) {
                 printWriter.println("<script>\n" + "    alert(\"Login failed\");\n" + "</script>");
                 System.out.println("Login failed");
+            } catch (InvalidKeySpecException e) {
+                throw new RuntimeException(e);
             }
         } else {
             System.out.println("Login failed");
