6ebda811a443a15bc79990d94c46ff45698b8713
Nguyễn Hoàng Hải <103479379+NgHgHai@users.noreply.github.com>
2023-09-24 02:31:00 +0700

    dẫn đến trang thông tin người dùng, bao gồm những danh sách, địa chỉ, yêu thích, đơn hàng,thông tin của user đó

--- src/main/java/hcmuaf/edu/vn/BikeEcommerce/controllers/userController/UserController.java
+++ src/main/java/hcmuaf/edu/vn/BikeEcommerce/controllers/userController/UserController.java
@@ -1,7 +1,13 @@
 package hcmuaf.edu.vn.BikeEcommerce.controllers.userController;
 
+import hcmuaf.edu.vn.BikeEcommerce.model.Address;
+import hcmuaf.edu.vn.BikeEcommerce.model.Favorite;
+import hcmuaf.edu.vn.BikeEcommerce.model.Order;
 import hcmuaf.edu.vn.BikeEcommerce.model.User;
 import hcmuaf.edu.vn.BikeEcommerce.model.sercurity.Token;
+import hcmuaf.edu.vn.BikeEcommerce.service.AddressService;
+import hcmuaf.edu.vn.BikeEcommerce.service.FavoriteService;
+import hcmuaf.edu.vn.BikeEcommerce.service.OrderService;
 import hcmuaf.edu.vn.BikeEcommerce.service.UserService;
 
 import javax.servlet.ServletException;
@@ -10,15 +16,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/user")
 public class UserController extends HttpServlet {
+    /**
+     * Lấy thông tin user
+     * URL: /user
+     * Method: GET
+     * require: token
+     */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Token token = (Token) req.getAttribute("token");
-        User user = UserService.getInstance().getUserByKey(token.getUserId());
-        req.setAttribute("user", user);
-        req.getRequestDispatcher("/dev/User.jsp").forward(req, resp);
+
+        if (token != null) {
+
+            User user = UserService.getInstance().getUserByKey(token.getUserId());
+            List<Address> addressList = AddressService.getInstance().getAllAddressByUserId(token.getUserId());
+            List<Order> orderList = OrderService.getInstance().getAllOrderByUserId(token.getUserId());
+            List<Favorite> favoriteList = FavoriteService.getInstance().getFavoriteByUserId(token.getUserId());
+            req.setAttribute("favoriteList", favoriteList);
+            req.setAttribute("addressList", addressList);
+            req.setAttribute("orderList", orderList);
+            req.setAttribute("user", user);
+        }
+        req.getRequestDispatcher("/user/User.jsp").forward(req, resp);
     }
 
     @Override
