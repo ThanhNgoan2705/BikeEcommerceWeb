0b791fed4bf1ab2f45d71002fcc67c9f023a6d90
Nguyễn Hoàng Hải <103479379+NgHgHai@users.noreply.github.com>
2023-12-14 15:57:47 +0700

    them cac thu vien can thiet, them lop CheckSig co 2 phuong thuc,  - checksignature // kiem tra chu ki co hop le khong  -getSeriOfCertByCMSSigData // lay seri tu CSMSigData

---
+++ src/main/java/hcmuaf/edu/vn/BikeEcommerce/service/digitSig/CheckSig.java
@@ -0,0 +1,42 @@
+package hcmuaf.edu.vn.BikeEcommerce.service.digitSig;
+
+import hcmuaf.edu.vn.BikeEcommerce.model.Order;
+import hcmuaf.edu.vn.BikeEcommerce.service.OrderService;
+import org.bouncycastle.cms.CMSException;
+import org.bouncycastle.cms.CMSSignedData;
+import org.bouncycastle.operator.OperatorCreationException;
+import vn.edu.atbmmodel.digitalsignature.SignInData;
+import vn.edu.atbmmodel.hash.Hash;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+public class CheckSig {
+
+    OrderService orderService = OrderService.getInstance();
+
+    public CheckSig() {
+    }
+
+    public boolean checkSignature(String orderId, byte[] sig) throws GeneralSecurityException, OperatorCreationException, CMSException {
+        Order order = orderService.getOrderById(orderId);
+        String data = order.toStringForHash();
+        Hash hashFunc = new Hash("SHA-256");
+        String hash = hashFunc.hash(data);
+        Boolean c = SignInData.verifyDetachedData(hash.getBytes(),sig);
+        return c;
+    }
+    public String getSeriOfCertByCMSSigData(byte[] sig) throws GeneralSecurityException, OperatorCreationException, CMSException {
+        CMSSignedData cmsSignedData = new CMSSignedData(sig);
+        String seri = cmsSignedData.getSignerInfos().getSigners().iterator().next().getSID().getSerialNumber().toString();
+        return seri;
+    }
+
+    public static void main(String[] args) throws IOException, GeneralSecurityException, OperatorCreationException, CMSException {
+        CheckSig checkSig = new CheckSig();
+        byte[] sig = new FileInputStream("C:\\Users\\H\\Documents\\k.sig").readAllBytes();
+
+        System.out.println( checkSig.getSeriOfCertByCMSSigData(sig));
+    }
+}
--- src/main/java/hcmuaf/edu/vn/BikeEcommerce/test.java
+++ src/main/java/hcmuaf/edu/vn/BikeEcommerce/test.java
@@ -1,22 +1,25 @@
 package hcmuaf.edu.vn.BikeEcommerce;
 
+import org.bouncycastle.operator.OperatorCreationException;
 import vn.edu.atbmmodel.key.KeyGen;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.math.BigInteger;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.PrivateKey;
-import java.security.SecureRandom;
+import java.security.*;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
 
 public class test {
-    public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchProviderException, IOException {
+    public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchProviderException, IOException, CertificateException, OperatorCreationException {
         //day la lenh tao ra seri co do dai 256 bit=64 ki tu hexa
         BigInteger serial = new BigInteger(256, new SecureRandom());
-        System.out.println(serial.toString(16).length());
         byte[] privateBytes = new FileInputStream("src/main/privateInfo/GreenLockPrivateKey.key").readAllBytes();
         PrivateKey privateKey = KeyGen.getInstance().getPrivateKeyformBytes(privateBytes);
-        System.out.println(privateKey.getAlgorithm());
+        byte[] publicBytes = new FileInputStream("src/main/privateInfo/GreenLockPublicKey.key").readAllBytes();
+        PublicKey publicKey = KeyGen.getInstance().getPublicKeyformBytes(publicBytes);
+
+        Certificate certificate = KeyGen.getInstance().genCertificate(privateKey,publicKey,"hoanghai",serial );
+        System.out.println(certificate);
     }
 }
