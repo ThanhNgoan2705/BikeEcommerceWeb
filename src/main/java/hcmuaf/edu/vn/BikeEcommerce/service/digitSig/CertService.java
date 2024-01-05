f74a25ac807462d9c6d1adbc87d3104f48561988
NguyenLeThuThao <20130409@st.hcmuaf.edu.vn>
2024-01-05 19:05:31 +0700

    key management

--- src/main/java/hcmuaf/edu/vn/BikeEcommerce/service/digitSig/CertService.java
+++ src/main/java/hcmuaf/edu/vn/BikeEcommerce/service/digitSig/CertService.java
@@ -3,8 +3,14 @@ package hcmuaf.edu.vn.BikeEcommerce.service.digitSig;
 import hcmuaf.edu.vn.BikeEcommerce.DAO.digitSig.CertDAO;
 import hcmuaf.edu.vn.BikeEcommerce.db.JDBIConnector;
 import hcmuaf.edu.vn.BikeEcommerce.model.digitSig.Cert;
+import hcmuaf.edu.vn.BikeEcommerce.model.digitSig.CertView;
+import hcmuaf.edu.vn.BikeEcommerce.model.digitSig.UserSeri;
 import org.jdbi.v3.core.Jdbi;
 
+import java.security.cert.X509Certificate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class CertService {
@@ -26,6 +32,16 @@ public class CertService {
         return jdbi.withExtension(CertDAO.class, dao -> dao.getCertBySeri(seri));
     }
 
+    public List<Cert> getByUserId(String userId) {
+        List<UserSeri> userSeris = UserSeriService.getInstance().getAllSeriOfUser(userId);
+        List<Cert> result = new ArrayList<>();
+        for (UserSeri u : userSeris) {
+            Cert certTemp = getBySeri(u.getSeri());
+            result.add(certTemp);
+        }
+        return result;
+    }
+
     public void insert(Cert cert) {
         jdbi.useExtension(CertDAO.class, dao -> dao.insertCert(cert));
     }
@@ -33,9 +49,30 @@ public class CertService {
 
     public static void main(String[] args) {
         CertService certService = CertService.getInstance();
-        Cert cert = certService.getBySeri("123");
-        System.out.println(cert);
+        List<Cert> certs = certService.getByUserId("94face32-ca88-4729-b3de-b34a0e390e48");
+        System.out.println(certs.size());
+        X509Certificate certificate = (X509Certificate) certs.get(0).getCertificate();
+        System.out.println(certificate.toString());
 
+//        List<CertView> certViews = new ArrayList<>();
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+//        Date dateNow = new Date();
+//        for (Cert c : certs) {
+//            CertView temp = new CertView();
+//            temp.getSeri(c.getSeri());
+//            X509Certificate certificate = (X509Certificate) c.getCertificate();
+//
+//            Date dateAfter = ((X509Certificate) certificate).getNotAfter();
+//            Date dateBefore = ((X509Certificate) certificate).getNotBefore();
+////            Instant instant = date.toInstant();
+////            LocalDate localDate = instant.atZone(ZoneId.systemDefault()).toLocalDate();
+////            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+//            temp.setName(String.valueOf(certificate.getSubjectDN()));
+//            temp.setStartDate(String.valueOf(certificate.getNotBefore()));
+//            temp.setEndDate(String.valueOf(certificate.getNotAfter()));
+//            certViews.add(temp);
+//        }
+//        System.out.println(certViews.size());
 
     }
 }
---
+++ src/main/java/hcmuaf/edu/vn/BikeEcommerce/service/digitSig/CertViewService.java
@@ -0,0 +1,48 @@
+package hcmuaf.edu.vn.BikeEcommerce.service.digitSig;
+
+import hcmuaf.edu.vn.BikeEcommerce.model.digitSig.Cert;
+import hcmuaf.edu.vn.BikeEcommerce.model.digitSig.CertView;
+import hcmuaf.edu.vn.BikeEcommerce.model.digitSig.RevocationCert;
+
+import java.security.cert.X509Certificate;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class CertViewService {
+
+    public static List<CertView> getCertViewFormUserId(String userId) {
+        List<CertView> certViews = new ArrayList<>();
+        List<Cert> certs = CertService.getInstance().getByUserId(userId);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        Date dateNow = new Date();
+        for (Cert c : certs) {
+            CertView temp = new CertView();
+            temp.setSeri(c.getSeri());
+            X509Certificate certificate = (X509Certificate) c.getCertificate();
+
+            Date dateAfter = ((X509Certificate) certificate).getNotAfter();
+            Date dateBefore = ((X509Certificate) certificate).getNotBefore();
+
+            LocalDate localDateDateAfter = dateAfter.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            LocalDate localDateDateBefore = dateBefore.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            temp.setName(String.valueOf(certificate.getSubjectDN()));
+            temp.setStartDate(localDateDateBefore.format(formatter));
+            temp.setEndDate(localDateDateAfter.format(formatter));
+            RevocationCert revocationCert = RevocationCertService.getInstance().getBySeri(c.getSeri());
+            if (revocationCert != null) {
+                temp.setStatus("Bị khóa");
+            } else if (dateNow.after(dateAfter)) {
+                temp.setStatus("Het han");
+            } else {
+                temp.setStatus("Hoat dong");
+            }
+            certViews.add(temp);
+        }
+        return certViews;
+    }
+}
